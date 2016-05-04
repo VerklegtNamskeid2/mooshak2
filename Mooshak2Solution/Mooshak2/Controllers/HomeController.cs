0fcc2530aca391816a66a6ef8c80ee70f8a64aa8
helga13 <helga13@ru.is>
2016-05-04 23:00:19 +0000

    working on authentication

--- Mooshak2Solution/Mooshak2/Controllers/HomeController.cs
+++ Mooshak2Solution/Mooshak2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mooshak2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,20 +7,55 @@ using System.Web.Mvc;
 
 namespace Mooshak2.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult TestAuth()
+        {
+            IdentityManager manager = new IdentityManager();
+            if (manager.RoleExists("Administrators"))
+            {
+                manager.CreateRole("Administrators");
+            }
+            if (manager.UserExsists("Admin@admin.com"))
+            {
+                ApplicationUser newAdmin = new ApplicationUser();
+                newAdmin.UserName = "admin@admin.com";
+                manager.CreateUser(newAdmin, "123456");
+            }
+
+            var user = manager.GetUser("admin@admin.com");
+            if (!manager.UserIsInRole(user.Id, "Administrators"))
+            {
+                manager.AddUserToRole(user.Id,"Administrators");
+            }
+
+            //var adminUserRoles = manager.GetUserRoles(User.id);
+
+            //adminUserRoles = manager.GetUserRoles(user.Id);
+
+            if(User.Identity.IsAuthenticated == true)
+            {
+                //var id = User.Identity.GetUserId();
+                //var myRoles = manager.GetUserRoles(id);
+            }
+
+            return null;
+        }
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+       
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ Mooshak2Solution/Mooshak2/Models/IdentityManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models
+{
+    public class IdentityManager
+    {
+        public bool RoleExists(string name)
+        {
+            return true;
+        }
+        public bool CreateRole(string name)
+        {
+            return true;
+        }
+        public bool UserExsists(string name)
+        {
+            return true;
+        }
+        public ApplicationUser GetUser(string name)
+        {
+            return null;
+        }
+        public bool CreateUser(ApplicationUser user, string password)
+        {
+            return true;
+        }
+        public bool AddUserToRole(string userId, string roleName)
+        {
+            return true;
+        }
+        public bool UserIsInRole(string userId, string roleName)
+        {
+            return true;
+        }
+        public void ClearUserRoles(string userId)
+        {
+            
+        }
+        public IList<string> GetUserRoles(string userId)
+        {
+            return null;
+        }
+    }
+}
