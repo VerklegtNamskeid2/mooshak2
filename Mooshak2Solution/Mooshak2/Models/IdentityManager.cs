655bf64719411e33a4906a3875b18a8b3ab3bffc
heidah <heidah13@ru.is>
2016-05-05 15:00:03 +0000

    working in Identity manager

--- Mooshak2Solution/Mooshak2/Models/IdentityManager.cs
+++ Mooshak2Solution/Mooshak2/Models/IdentityManager.cs
@@ -17,35 +17,56 @@ namespace Mooshak2.Models
         }
         public bool CreateRole(string name)
         {
-            return true;
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            var idResult = roleManager.Create(new IdentityRole(name));
+            return idResult.Succeeded;
         }
         public bool UserExsists(string name)
         {
-            return true;
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            return userManager.FindByName(name) != null;
         }
         public ApplicationUser GetUser(string name)
         {
-            return null;
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            return userManager.FindByName(name);
         }
         public bool CreateUser(ApplicationUser user, string password)
         {
-            return true;
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = userManager.Create(user, password);
+            return idResult.Succeeded;
         }
         public bool AddUserToRole(string userId, string roleName)
         {
-            return true;
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = userManager.AddToRole(userId, roleName);
+            return idResult.Succeeded;
         }
         public bool UserIsInRole(string userId, string roleName)
         {
-            return true;
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var result = userManager.IsInRole(userId, roleName);
+            return result;
         }
         public void ClearUserRoles(string userId)
         {
-            
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            var user = userManager.FindById(userId);
+            var currentRoles = new List<IdentityUserRole>();
+            currentRoles.AddRange(user.Roles);
+            foreach (var role in currentRoles)
+            {
+                var r = roleManager.FindById(role.RoleId);
+                userManager.RemoveFromRole(userId, r.Name);
+            }
         }
         public IList<string> GetUserRoles(string userId)
         {
-            return null;
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var result = userManager.GetRoles(userId);
+            return result;
         }
     }
 }
